d7d1e032b5478e3abd526f097423aab6b2d95fd1
Stasshe <133995608+Stasshe@users.noreply.github.com>
2024-08-31 21:13:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ document.getElementById('transaction-form').addEventListener('submit', function(
         displayTopCustomers();
     }).catch((error) => {
         console.error('Error:', error);
-        document.getElementById('transaction-status').innerText = 'Transaction successful';
+        document.getElementById('transaction-status').innerText = 'Transaction 56successful';
     });
 });
 
