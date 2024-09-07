fa470f2c436491d1bd23355e03281a9dc3dbe7c8
Stasshe <133995608+Stasshe@users.noreply.github.com>
2024-09-07 11:58:15 +0900

    Update dataclean.js

--- dataclean.js
+++ dataclean.js
@@ -1,3 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeBgPZmKE-qRm_TANkWK2_4iBgIjMPNys",
+    authDomain: "bankdbfirebase.firebaseapp.com",
+    databaseURL: "https://bankdbfirebase-default-rtdb.firebaseio.com",
+    projectId: "bankdbfirebase",
+    storageBucket: "bankdbfirebase.appspot.com",
+    messagingSenderId: "732737141001",
+    appId: "1:732737141001:web:6d4ac9a70dc68d39d3fbd8",
+    measurementId: "G-FXFE6CNBER"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
 
 
 
